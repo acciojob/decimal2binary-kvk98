60834bddba6bfb04b879cea45581d45b304c9ac9
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-18 14:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let binaryNum = "";
+  while (num >=1) {
+  	binaryNum = binaryNum + (num%2);
+	  num = Math.floor(num/2);
+  }
+	console.log(binaryNum);
 }
 
 window.decimalToBinary = decimalToBinary;
